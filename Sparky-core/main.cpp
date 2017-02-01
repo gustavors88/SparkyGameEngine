2127840c7485ffb71e0b50d65225a1f914c40826
gustavors88 <gustavors88@gmail.com>
2017-02-01 23:12:39 +0900

    Added Glew

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -7,21 +7,25 @@ int main() {
 	Window window("Sparky!", 950, 540);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	while (!window.closed()) {
-		std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
 		window.clear();
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2d(-0.5f, -0.5f);
 		glVertex2d(-0.5f,  0.5f);
 		glVertex2d( 0.5f,  0.5f);
 		glVertex2d( 0.5f, -0.5f);
 		glEnd();
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
+
 		window.update();
 	}
-
-	
 	return 0;
 }
